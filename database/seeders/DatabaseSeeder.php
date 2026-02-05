93e8acef40a64fb3a0e1ecc1f79698d07c5f4d66
naufalix <naufalulinnuha40@gmail.com>
2026-02-05 21:43:44 +0700

    update data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use App\Models\Office;
+use App\Models\Presence;
 use App\Models\User;
+use File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,7 +22,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         Admin::create([
-            "name" => "Naufal Ulinnuha (Admin)",  
+            "name" => "Naufal Ulinnuha",  
             "email" => "admin@naufal.dev",  
             "password" => bcrypt('admin'),
             "role" => 'admin'
@@ -34,11 +36,40 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::create([
-            "name" => "Naufal Ulinnuha (Pegawai)",  
+            "name" => "Naufal Ulinnuha",  
             "email" => "user@naufal.dev",  
             "email_verified_at" => now(),  
             "position" => "Web Developer",  
             "password" => bcrypt('user')
         ]);
+        User::create([
+            "name" => "Nada",  
+            "email" => "nada@naufal.dev",  
+            "email_verified_at" => now(),  
+            "position" => "Project Manager",  
+            "password" => bcrypt('nada')
+        ]);
+        User::create([
+            "name" => "Stevia",  
+            "email" => "stevia@naufal.dev",  
+            "email_verified_at" => now(),  
+            "position" => "Kucing",  
+            "password" => bcrypt('stevia')
+        ]);
+
+        $presences = json_decode(File::get("database/data/presences.json"));
+        foreach ($presences as $key => $value) {
+            Presence::create([
+                "user_id" => $value->user_id,
+                "presence_date" => $value->presence_date,
+                "time_in" => $value->time_in,
+                "time_out" => $value->time_out,
+                "location_in" => "-7.7654442,112.5906469",
+                "location_out"  => "-8.1078426,112.92248",
+                "image_in"  => "default.jpg",
+                "image_out"  => "default.jpg",
+                "status"  => 2,
+            ]);
+        }
     }
 }
